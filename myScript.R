be047dcf00c96494e415b894730e09a8f883c5f0
issyzac <issyzac.iz@gmail.com>
2021-02-12 00:20:10 +0300

    First Commit

---
+++ myScript.R
@@ -0,0 +1,15 @@
+library(dplyr)
+
+# Creating Vectors
+seq(2,3, by=0.5)
+rep(1:2, times=5)
+2:6
+
+#Vector Functions
+my_object <- rep(1:2, times=5)
+sort(my_object)
+table(my_object)
+rev(my_object)
+unique(my_object)
+
+
